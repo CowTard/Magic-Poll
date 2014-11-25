85accf6c9fde4e434177578b78762ec7b92ebb78
Sukigu <Sukigu@users.noreply.github.com>
2014-11-25 18:55:08 +0000

    Botões para registo/início de sessão

--- script/main.js
+++ script/main.js
@@ -1,5 +1,16 @@
-$(document).ready( function() {
-	
-	$('#Registerbox').hide();
+$(document).ready(function() {
+	$('#registerform').hide();
+	document.getElementById('registerform').onclick = hideLoginForm;
+	$('#loginlink').onclick = hideRegisterForm;
 });
 
+function hideRegisterForm() {
+alert('ola');
+	$('#registerform').hide();
+	$('#loginform').show();
+}
+
+function hideLoginForm() {
+	$('#loginform').hide();
+	$('#registerform').show();
+}
