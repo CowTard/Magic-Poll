d866567458037ff7818ae8bdff2ad341a42d8f77
Tiago Neto <ei12109@fe.up.pt>
2014-11-25 16:49:13 +0000

    Alguns erros corrigidos.

--- panel/insertPoll.php
+++ panel/insertPoll.php
@@ -5,19 +5,29 @@
 	
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$title = security($_POST["title"]);
-		$option1 = security($_POST["Option1"]);
-		$option2 = security($_POST["Option2"]);
 
 		$db = new PDO('sqlite:../db/polls.db');
 		$dbPrepared = $db->prepare('INSERT INTO Poll(IDuser,Title,Votes) values(?,?,?)');
+		$last_id = $db->lastInsertId('ID');
+		var_dump($last_id);
 		$dbPrepared->execute(array($_SESSION["ID"],$title,0));
 		$dbPrepared->fetchAll();
 
+
+		// Inserting options in database 
 		$db = new PDO('sqlite:../db/polls.db');
 		$dbPrepared = $db->prepare('INSERT INTO Options(IDPoll,OptionText) values(?,?)');
-		$dbPrepared->execute(array($last_id,$option1));
-		$dbPrepared->fetchAll();
 
+		$tamanhoPost = count($_POST);
+		$tamanhoPost = $tamanhoPost - 1;
+
+		$i = 1;
+		while($i <= $tamanhoPost){
+			$id = "Option" . $i;
+			$dbPrepared->execute(array($last_id,security($_POST[$id])));
+			$dbPrepared->fetchAll();
+			$i = $i + 1;
+		}
 		header("Location: http://localhost:8888/LTW/panel/createPoll.php");
 
 	}
