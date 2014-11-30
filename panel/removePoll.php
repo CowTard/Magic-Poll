85ccef8c859e67d63b0053ef6dbd99ff8e325258
Gustavo Silva <Sukigu@users.noreply.github.com>
2014-11-30 06:45:57 +0000

    #5 corrigido (imagens são eliminadas quando uma votação é removida)

--- panel/removePoll.php
+++ panel/removePoll.php
@@ -1,10 +1,7 @@
 <?php
-	session_start();
 	require 'dashboard_header.php';
 
-
 	$db = new PDO('sqlite:../db/polls.db');
-
 
 	// COMO NAO E POSSIVEL REVERTER O HASH, OBTEM-SE O ID REAL ATRAVES DE UMA BUSCA A BASE DE DADOS.
 
@@ -13,21 +10,23 @@
 	$item = $dbPrepared->fetch();
 
 	$realID = $item['ID'];
+	
+	// ELIMINAR IMAGEM ASSOCIADA
+	$imagename = $item['ImageName'];
+	var_dump($imagename);
+	unlink('../uploadedImages/' . $imagename);
 
 	// APAGAR REGISTO NA TABELA POLL
-
 	$dbPrepared = $db->prepare('DELETE FROM Poll WHERE EncodedID = ?');
 	$dbPrepared->execute(array($_GET['id']));
 	$item = $dbPrepared->fetch();
 
 	// APAGAR REGISTO NA TABELA OPTIONS
-
 	$dbPrepared = $db->prepare('DELETE FROM Options WHERE IDPoll = ?');
 	$dbPrepared->execute(array($realID));
 	$item = $dbPrepared->fetch();
 
 	// APAGAR REGISTO NA TABELA VOTES
-
 	$dbPrepared = $db->prepare('DELETE FROM Votes WHERE IDPoll = ?');
 	$dbPrepared->execute(array($realID));
 	$item = $dbPrepared->fetch();
