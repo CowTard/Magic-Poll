76ad99420849ea1891a896d4d766edd0b0b0eb54
Tiago Neto <ei12109@fe.up.pt>
2014-12-05 02:12:08 +0000

    Corrigido i think.
    Com essa margin negativa..xd

--- index.php
+++ index.php
@@ -73,11 +73,54 @@
 		</div>
 
   		<!--  STARTING FOOTER -->
-		<div class="footer">
-			<div class="container">
-				<p class="text-center">© MagicPoll 2014 <a href="www.fe.up.pt">MIEIC@FEUP</a></p>
-		    </div>
-		</div>
+</div>
+<!-- Modal for contact form -->
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="modalform" aria-hidden="true" id="modalform">
+<div class="modal-dialog modal-sm">
+        <div class="modal-content">
+          <div class="modal-content">
+                <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Dont be too harsh on us. We are trying each day to improve this site.</h4>
+                </div>
+                <div class="modal-body">
+                     <form id="contact-form" method=""> 
+                     <!-- <form id="requestacallform" class="form-horizontal" method=""> -->
+
+                                <div class="form-group">
+                                	<label for="name">Name</label>                      
+                                    <input id="name" type="text" class="form-control" placeholder="Your name" name="name" required/>
+                              	</div>
+                              	<div class="form-group">
+                                	<label for="subject">Subject</label>                     
+                                    <input id="subject" type="text" class="form-control" placeholder="Brief description of you want to say to us" name="subject" required/>
+                              	</div>
+                                <div class="form-group">
+                                	<label for="email">Email</label>                        
+                                    <input id="email" type="text" class="form-control" placeholder="Email" name="email" required/>
+                              	</div>
+								<div class="form-group">
+									<label for="message">Message</label>
+										<textarea class="form-control" placeholder="Your message here.." name="message" id="message"></textarea>
+								</div>
+
+                            
+                  </div>
+              <div class="modal-footer">
+                <button type="submit" class="btn btn-success pull-right">Send It!</button>
+                </form>
+              </div>          
+        </div>
+        </div>
+      </div>
+    </div>
+
+<div class="footer">
+   <div class="container">
+      <p class="text-center"><a data-toggle="modal" data-target="#modalform" href="#">[Support]</a>
+      <p class="text-center">© MagicPoll 2014 <a href="www.fe.up.pt">MIEIC@FEUP</a></p>
+   </div>
+</div>
 
 		<!-- BETTER PERFORMANCE -->
 		<script src="./script/jquery-1.11.1.min.js"></script>
